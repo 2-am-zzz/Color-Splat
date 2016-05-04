5c207b708f515ad118db77a4d3ddecacbe9128ab
Kevin Serrano <kevgagser@gmail.com>
2016-05-04 10:33:11 -0700

    Add conversion functions from RGB to HEX and vice versa. Add gitignore.

--- lib/color_splat.rb
+++ lib/color_splat.rb
@@ -1,3 +1,16 @@
 module ColorSplat
+  class Core
+    def self.hex_to_rgb(hexcode)
+      [hexcode[0..1].to_i(16), hexcode[2..3].to_i(16), hexcode[4..5].to_i(16)]
+    end
+
+    def self.rgb_to_hex(rgbcode)
+      hexcode = ""
+      rgbcode.each do |component|
+        hexcode += component.to_s(16)
+      end
+      hexcode
+    end
+  end
 end
 require_relative 'color_splat/flags'
